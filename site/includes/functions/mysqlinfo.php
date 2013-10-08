4a7b4d0d6aa2593c728b32d1d3081a089a4c2e57
mshred <mshred@securityspot.org>
2013-10-08 10:39:15 +0200

    Tweak the directory structure a little and begin work on the registration system

---
+++ site/includes/functions/mysqlinfo.php
@@ -0,0 +1,8 @@
+<?php
+
+	function get_mysqlinfo() {
+		$dbinfo = array('host' => '','username' => '','pass' => '','db' => '');
+		return $dbinfo;
+	}
+
+?>
--- site/register.php
+++ site/register.php
@@ -6,18 +6,31 @@
    Echo out data to the login page to be user friendly with a restration success message
    (If you are feeling fancy, have the script email a verification code)
    Make sure to close all MySQL Connections to prevent too many sessions error
+CREATE TABLE `a7057662_db`.`login` (
+`id` INT NOT NULL AUTO_INCREMENT ,
+`username` VARCHAR( 25 ) NOT NULL ,
+`passwordhash` VARCHAR( 50 ) NOT NULL ,
+`salt` VARCHAR( 32 ) NOT NULL ,
+`registration_email` VARCHAR( 50 ) NOT NULL ,
+`registration_date` DATETIME NOT NULL ,
+`registration_ip` INT( 20 ) NOT NULL ,
+`hide_email` VARCHAR( 10 ) NOT NULL ,
+`group` INT( 2 ) NULL ,
+`access` INT( 2 ) NULL ,
+`security_question` TEXT NOT NULL ,
+`security_answer` TEXT NOT NULL ,
+`last_login_ip` INT( 20 ) NOT NULL ,
+`verifyhash` VARCHAR( 50 ) NOT NULL ,
+`signature` TEXT NOT NULL ,
+`birthday` DATETIME NOT NULL ,
+`timezone` VARCHAR( 30 ) NOT NULL ,
+PRIMARY KEY ( `id` )
+) ENGINE = MYISAM
+
+
 */
 
 <?php
-
-/* Set Database Information */
-$db_host = "";
-$db_name = "";
-$db_user = "";
-$db_pass = "";
-// Connect to server and select databse.
-mysql_connect("$db_host", "$db_user", "$db_pass")or die("cannot connect");
-mysql_select_db("$db_name")or die("cannot select DB");
 
 // Generating a random salt value
 function unique_md5() {
@@ -25,44 +38,32 @@ function unique_md5() {
     return md5(uniqid(mt_rand(), true));
 }
 
-$email    = stripslashes(mysql_real_escape_string($_POST['email']));
-$password = md5(stripslashes(mysql_real_escape_string($_POST['password'])));
-$fname    = stripslashes(mysql_real_escape_string($_POST['firstname']));
-$lname    = stripslashes(mysql_real_escape_string($_POST['lastname']));
-$nick     = stripslashes(mysql_real_escape_string($_POST['nickname']));
-$gender   = stripslashes(mysql_real_escape_string($_POST['gender']));
-$error    = false;
+/* in depth form validation needs to happen here */
+
+$fname = db_escape($_POST['fname']);
+$lname = db_escape($_POST['lname');
+$username = db_escape($_POST['username']);
+$password = db_escape($_POST['password']);
+$registration_email = db_escape($_POST['email']);
+$registration_ip = $_SERVER['REMOTE_ADDR'];
+$registration_date = date();	//finish this
+$hide_email = db_escape($_POST['hide_email']);
+$group = 1; 			//or whatever default group is
+$access = 0;			//or whatever default access level is
+$security_question = db_escape($_POST['security_question']);
+$security_answer = db_escape($_POST['security_answer']);
+$signature = db_escape($_POST['signature']);
+$birthday = db_escape($_POST['birthday']);
+$timezone = db_escape($_POST['timezone']);
+//$verifyhash = create_verifyhash();
 
 $salt     = unique_md5(); // Store random salt in $salt
-$newpass  = md5("$password"."$salt");
+$passwordhash  = hash('sha256',($password . $salt));
 
-$sql_check    = "SELECT * FROM login where email='$email'";
-$result_check = mysql_query($sql_check);
-$count = mysql_num_rows($result_check);
 
-// If result matched $username and $password, table row must be 1 row
+/* After everything is validated and verified
+	insert everything into the db */
 
-if($count > 0) $error = true;
-if(is_null($email) || $email == '') $error = true;
-if(is_null($password) || $password == '') $error = true;
-if(is_null($fname) || $fname == '') $error = true;
-if(is_null($lname) || $lname == '') $error = true;
-if(is_null($nick) || $nick == '') $error = true;
-if(is_null($gender) || $gender == '') $error = true;
 
-if (filter_var($email, FILTER_VALIDATE_EMAIL) && $error == false) {
-    $sql      = "INSERT INTO login (email, password, salt, fname, lname, nick, gender) VALUES ('$email', '$newpass', '$salt', '$fname', '$lname', '$nick', '$gender')";
-    $result   = mysql_query($sql);
-    mysql_close(mysql_connect("$db_host", "$db_user", "$db_pass"));
-}
-  if($result) {
-    echo("<html>Thank you for registering an account with us.</br>You may <a href=\"login.html\">login</a> with your email: $email and password now!</b>");
-  }
-  else { 
-    $error = true;
-  }
-if($error == true) {
-  echo("<html><b>There was an error registering your account!</br>Please <a href=\"register.html\">go back and try again</a>!</html>");
-  mysql_close(mysql_connect("$db_host", "$db_user", "$db_pass"));
-}
+
 ?>
