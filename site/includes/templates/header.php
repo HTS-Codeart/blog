3046221e8f92f310791ac80d9d3beb56c767eed6
= <=>
2013-10-08 04:17:44 -0500

    Added Dynamic Header

--- site/includes/templates/header.php
+++ site/includes/templates/header.php
@@ -1,7 +1,10 @@
 <?php
-	/* Possible header template */
-?>
+$header = '<head>
+  <title>HTS-Codeart</title>
+  <link rel="stylesheet" type="text/css" href="/css/style.css" />
+  <script type="text/javascript" src="/files/js/jquery.min.js"></script>
+  <script type="text/javascript" src="/files/js/image_slide.js"></script>
+</head>';
 
-<html>
-<head></head>
-<body>
+echo $header;
+?>
