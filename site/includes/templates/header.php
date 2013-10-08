c1e3caef4b472eef11bc2ecfd35aec690d0be864
Ninjex <ixidmrixi@yahoo.com>
2013-10-08 04:26:09 -0500

    Fixed js includes

--- site/includes/templates/header.php
+++ site/includes/templates/header.php
@@ -2,8 +2,8 @@
 $header = '<head>
   <title>HTS-Codeart</title>
   <link rel="stylesheet" type="text/css" href="/css/style.css" />
-  <script type="text/javascript" src="/files/js/jquery.min.js"></script>
-  <script type="text/javascript" src="/files/js/image_slide.js"></script>
+  <script type="text/javascript" src="/js/jquery.min.js"></script>
+  <script type="text/javascript" src="/js/image_slide.js"></script>
 </head>';
 
 echo $header;
