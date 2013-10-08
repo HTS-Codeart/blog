45cf895bbb56f5ae9521bc319c783b5f22f3a502
= <=>
2013-10-08 00:44:15 -0500

    Removed /files/*

--- site/files/php/login.php
+++
@@ -1,4 +0,0 @@
-/* Check login form from /login.html
-   Use proper sanitation: stripslashes(mysql_real_escape_string($data));
-   Make sure to hash the password before matching (If using a salt, add the salt value accordingly)
-*/
