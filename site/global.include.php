c0fd5a479b55fb56e69c646bd3559bffd0917b44
= <=>
2013-10-08 22:11:19 -0500

    Fixed global includes - index.html

--- site/global.include.php
+++ site/global.include.php
@@ -5,9 +5,18 @@
    You can include the file here, as well as other files, so that all we need is to add this one file to static pages.
 */
 
-include './includes/templates/header.php';
-include './includes/templates/footer.php';
-include './includes/templates/sidebar.php';
-include './includes/templates/navbar.php';
-// Shitty but works for now
+DEFINE('SITE_ROOT',getcwd());
+DEFINE('INCLUDES',SITE_ROOT . '/includes');
+DEFINE('HEADER',INCLUDES . '/templates/header.php');
+DEFINE('FOOTER',INCLUDES . '/templates/footer.php');
+DEFINE('SIDEBAR',INCLUDES . '/templates/sidebar.php');
+DEFINE('NAVBAR',INCLUDES . '/templates/navbar.php');
+DEFINE('FUNCTIONS',INCLUDES . '/functions');
+
+
+
+/* Require all the required shiz here */
+require_once(FUNCTIONS . '/mysql.php');
+require_once(FUNCTIONS . '/auth.php');
 ?>
+
