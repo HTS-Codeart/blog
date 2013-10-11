302290dfc9a671d1eb18cb9e4813eee44b4d75a8
= <=>
2013-10-11 13:24:00 -0500

    Fixed typo

--- site/errors/404.php
+++ site/errors/404.php
@@ -2,9 +2,10 @@
 
 	require_once('../global.include.php');
 	require_once(HEADER);
- 	require_once(NAVBAR;
+ 	require_once(NAVBAR);
  	require_once(SIDEBAR);
 
 	echo('<br>Temporary 404 page<br>');
 
 	require_once(FOOTER);
+?>
