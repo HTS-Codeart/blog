434bf4c3d77305a90c272bc4d588b85b5ccf965e
mShred <mshred@hackthissite.org>
2013-10-11 20:04:17 +0200

    Fixed last bug in temporary custom 404.php page

--- site/errors/404.php
+++ site/errors/404.php
@@ -1,10 +1,9 @@
 <?php
 
-	/* Not sure if this is gonna work or not. But I figured we'd try and create our own custom 404 page.
-		Might not work since the host is shit, but once moved it'll definitely work. */
-
 	require_once('../global.include.php');
 	require_once(HEADER);
+ 	require_once(NAVBAR;
+ 	require_once(SIDEBAR);
 
 	echo('<br>Temporary 404 page<br>');
 
