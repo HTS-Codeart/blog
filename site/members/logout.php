3fb9b1334b1bf15bd289cf879b323cc651a575dc
mShred <mshred@hackthissite.org>
2013-10-11 08:20:42 +0200

    Fix small require_once() bug in /members/register.php

--- site/members/logout.php
+++ site/members/logout.php
@@ -1,1 +1,3 @@
-/* Destroy the users session in this script */
+/* Destroy the users session in this script
+	Make sure we prevent CSRF attacks here. We can't have a link pointing to this file and have this script just destroy the session.
+	There needs to be some middle ground or another hoop to jump through. */
